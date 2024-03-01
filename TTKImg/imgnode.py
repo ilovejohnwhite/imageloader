e6efa1bf2fe2fd9892ce54585631e5cc7a87fb34
ilovejohnwhite <98855624+ilovejohnwhite@users.noreply.github.com>
2024-02-29 22:53:49 -0500

    Update imgnode.py

--- TTKImg/imgnode.py
+++ TTKImg/imgnode.py
@@ -48,7 +48,7 @@ class TTKImageTracer:
                     },
                 }
 
-    CATEGORY = "TatToolkit/Billy Goncho's Wild Ride"
+    CATEGORY = "TatToolkit"
     ''' Return order:
         width(int), height(int)
     '''
