a9a5eaf7786d6a844990ebe6aaf3f3b498491642
ilovejohnwhite <98855624+ilovejohnwhite@users.noreply.github.com>
2024-02-29 23:00:57 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
-from .node_mappings import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+import importlib
+import os
+
+node_list = [ #Add list of .py files containing nodes here
+    "Extra Nodes"
+]
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+for module_name in node_list:
+    imported_module = importlib.import_module(".{}".format(module_name), __name__)
+
+    NODE_CLASS_MAPPINGS = {**NODE_CLASS_MAPPINGS, **imported_module.NODE_CLASS_MAPPINGS}
+    NODE_DISPLAY_NAME_MAPPINGS = {**NODE_DISPLAY_NAME_MAPPINGS, **imported_module.NODE_DISPLAY_NAME_MAPPINGS}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
